f8b3cd4fd84ab1249798fbe2e88c2cd27f86a590
Cole Brown <cole@protocol.ai>
2019-02-08 12:52:57 -0500

    Implement services and bootstrapping

--- testlab/node/p2pd/p2pd.go
+++ testlab/node/p2pd/p2pd.go
@@ -5,16 +5,18 @@ import (
 
 	napi "github.com/hashicorp/nomad/api"
 	"github.com/libp2p/testlab/utils"
+	"github.com/sirupsen/logrus"
 )
 
 type P2pdNode struct{}
 
 func (n *P2pdNode) Task(options map[string]string) *napi.Task {
 	task := napi.NewTask("p2pd", "exec")
-	task.SetConfig("command", "/usr/local/bin/p2pd")
-	task.SetConfig("args", []string{
+	command := "/usr/local/bin/p2pd"
+	args := []string{
 		"-listen", "/ip4/${NOMAD_IP_p2pd}/tcp/${NOMAD_PORT_p2pd}",
-	})
+	}
+
 	res := napi.DefaultResources()
 	res.Networks = []*napi.NetworkResource{
 		&napi.NetworkResource{
@@ -26,6 +28,12 @@ func (n *P2pdNode) Task(options map[string]string) *napi.Task {
 	}
 	task.Require(res)
 
+	p2pdSvc := &napi.Service{
+		Name:      "p2pd",
+		PortLabel: "p2pd",
+	}
+	task.Services = append(task.Services, p2pdSvc)
+
 	if cid, ok := options["Cid"]; ok {
 		url := fmt.Sprintf("https://gateway.ipfs.io/ipfs/%s", cid)
 		task.Artifacts = []*napi.TaskArtifact{
@@ -34,8 +42,36 @@ func (n *P2pdNode) Task(options map[string]string) *napi.Task {
 				RelativeDest: utils.StringPtr("p2pd"),
 			},
 		}
-		task.SetConfig("command", "p2pd")
+		command = "p2pd"
 	}
+
+	if service, ok := options["Service"]; ok {
+		if service == "p2pd" {
+			logrus.Error("p2pd already exports service \"p2pd\"")
+		} else {
+			svc := &napi.Service{
+				Name:      service,
+				PortLabel: "libp2p",
+			}
+			task.Services = append(task.Services, svc)
+
+		}
+	}
+
+	if bootstrap, ok := options["Bootstrap"]; ok {
+		tmpl := fmt.Sprintf("BOOTSTRAP_PEERS={{range $index, $service := service \"%s\"}}{{if ne $index 0}},{{end}}/ip4/{{$service.Address}}/tcp/{{$service.Port}}{{end}}", bootstrap)
+		env := true
+		template := &napi.Template{
+			EmbeddedTmpl: &tmpl,
+			DestPath:     utils.StringPtr("bootstrap_peers.env"),
+			Envvars:      &env,
+		}
+		task.Templates = append(task.Templates, template)
+		args = append(args, "-bootstrapPeers", "$")
+	}
+
+	task.SetConfig("command", command)
+	task.SetConfig("args", args)
 
 	return task
 }
